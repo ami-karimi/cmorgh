65af7126fad802fbd5fe2718453ac499dec1ed98
Amir <takfashomal@gmail.com>
2023-10-16 18:16:38 +0330

    updatedd

--- pages/download.vue
+++ pages/download.vue
@@ -242,7 +242,7 @@
         <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">دانلود پروفایل ها ضروری میباشد پس از نصب برنامه بایستی به برنامه openvpn اضافه کنید</p>
 
 
-        <a href="https://arta20.xyz/download/openvpn.zip?t=8" class="w-full mb-2 sm:w-auto bg-gray-800 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700">
+        <a href="https://arta20.xyz/download/openvpn.zip?t=9" class="w-full mb-2 sm:w-auto bg-gray-800 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700">
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi ml-3 w-7 h-7 bi-cloud-arrow-down-fill" viewBox="0 0 16 16"> <path d="M8 2a5.53 5.53 0 0 0-3.594 1.342c-.766.66-1.321 1.52-1.464 2.383C1.266 6.095 0 7.555 0 9.318 0 11.366 1.708 13 3.781 13h8.906C14.502 13 16 11.57 16 9.773c0-1.636-1.242-2.969-2.834-3.194C12.923 3.999 10.69 2 8 2zm2.354 6.854-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 1 1 .708-.708L7.5 9.293V5.5a.5.5 0 0 1 1 0v3.793l1.146-1.147a.5.5 0 0 1 .708.708z"/> </svg>
           <div class="text-right">
             <div class="mb-1 text-xs">دانلود با</div>
